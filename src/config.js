b68595e856a7d4bdb1caca24e24d5cd386511e4f
Muhiddin Kurbonov <muhiddindc@gmail.com>
2021-05-16 16:23:14 -0700

    sc

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 80,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
   TEST_DB_URL: process.env.TEST_DB_URL,
